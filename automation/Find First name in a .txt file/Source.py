54d72aada00ace08089aa0eaddbebb59009fd00f
KareemHassan <kareem99710@gmail.com>
2025-02-26 22:01:31 +0200

    Change name input position

--- automation/Find First name in a .txt file/Source.py
+++ automation/Find First name in a .txt file/Source.py
@@ -1,10 +1,10 @@
 import sys
 
 file_path = input("Enter the text file path: ")
-first_name = input("Enter the first name to count: ")
 
 try:
     with open(file_path, 'r', encoding='utf-8') as file:
+        first_name = input("Enter the first name to count: ")
         lines = file.readlines()
         count = sum(1 for line in lines if line.split(',')[0].split()[0] == first_name)
     print(f"The first name '{first_name}' appears {count} times in the file.")
